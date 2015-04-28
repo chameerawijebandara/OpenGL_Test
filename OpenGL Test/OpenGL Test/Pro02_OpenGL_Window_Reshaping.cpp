3a369ebfaf2f9b778876ef634f945bdd34f48bdb
Chameera Wijebandara <chameera@alliontechnologies.com>
2015-04-28 13:43:29 +0530

    Pro02_OpenGL_Window_Reshaping is completed

--- OpenGL Test/OpenGL Test/Pro02_OpenGL_Window_Reshaping.cpp
+++ OpenGL Test/OpenGL Test/Pro02_OpenGL_Window_Reshaping.cpp
@@ -1,4 +1,4 @@
-#include <GL/glew.h>
+﻿#include <GL/glew.h>
 #include <GL/glut.h>
 
 void display()
@@ -9,6 +9,15 @@ void display()
 	glFlush();
 }
 
+void reshape (int width, int height) {  
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height); // Set our viewport to the size of our window  
+	glMatrixMode(GL_PROJECTION); // Switch to the projection matrix so that we can manipulate how our scene is viewed  
+	glLoadIdentity(); // Reset the projection matrix to the identity matrix so that we don't get any artifacts (cleaning up)  
+	gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 100.0); // Set the Field of view angle (in degrees), the aspect ratio of our window, and the new and far planes  
+	glMatrixMode(GL_MODELVIEW); // Switch back to the model view matrix, so that we can start drawing shapes correctly  
+}  
+
+
 int main (int argc, char **argv)
 {
 	glutInit(&argc, argv);
@@ -18,5 +27,6 @@ int main (int argc, char **argv)
 	glutCreateWindow("Reshap");
 
 	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
 	glutMainLoop();
 }
