0d1308bf28b8fcb776735a8c8cdf05e349f04280
Chameera Wijebandara <chameera@alliontechnologies.com>
2015-04-28 09:47:22 +0530

    Add basic functions from previous program

---
+++ OpenGL Test/OpenGL Test/Pro02_OpenGL_Window_Reshaping.cpp
@@ -0,0 +1,22 @@
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+void display()
+{
+	glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	glFlush();
+}
+
+int main (int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Reshap");
+
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
