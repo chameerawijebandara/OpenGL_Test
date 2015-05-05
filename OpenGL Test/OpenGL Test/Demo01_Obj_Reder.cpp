c691a7833f5c1dff690b905f5a770fd580370d3b
chameerawijebandara <chameerawijebandara@gmail.com>
2015-05-05 09:33:59 +0530

    Pro09 works fine

---
+++ OpenGL Test/OpenGL Test/Demo01_Obj_Reder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<memory>
+
+int main()
+{
+	std::cout << sizeof( wchar_t ) << std::endl;
+
+	//std::weak_ptr<std::string> pSptr1;
+	//std::auto_ptr<std::string> pSptr2( pSptr1 );
+	//std::auto_ptr<std::string> pSptr3 = pSptr2;
+
+	//std::cout << *pSptr1 << std::endl;
+	//std::cout << *pSptr2 << std::endl;
+	//std::cout << *pSptr3 << std::endl;
+}
